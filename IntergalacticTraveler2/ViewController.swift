3a70916f896d94109afe981b7cc5a04ee47fb1b1
vdelgado1407 <vdelgado.vd86@gmail.com>
2019-11-06 11:33:20 -0600

    Initial Commit

---
+++ IntergalacticTraveler2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntergalacticTraveler2
+//
+//  Created by Victor Delgado on 11/6/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
