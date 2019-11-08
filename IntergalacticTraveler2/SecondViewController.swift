9f2f2a8f0db3f3c44d19d70760e0b2bf98db0dbc
vdelgado1407 <vdelgado.vd86@gmail.com>
2019-11-08 11:27:20 -0600

    Added outlet for ImageView

---
+++ IntergalacticTraveler2/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  IntergalacticTraveler2
+//
+//  Created by Victor Delgado on 11/8/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var startImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
