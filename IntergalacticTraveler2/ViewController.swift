0c237924bc7328ae3ef99fa664d031df78a5bea6
vdelgado1407 <vdelgado.vd86@gmail.com>
2019-11-08 11:04:08 -0600

    Added outlet to TextField and ImageView to ViewController2

--- IntergalacticTraveler2/ViewController.swift
+++ IntergalacticTraveler2/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
